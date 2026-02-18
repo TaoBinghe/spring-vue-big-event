44db39f8146dd1f06a780854417504f9edd396ee
BingheTao <1741893518@qq.com>
2026-02-18 16:59:54 +0800

    Interceptor for other address (exclude login and register)

---
+++ src/main/java/com/binghetao/interceptors/LoginInterceptor.java
@@ -0,0 +1,26 @@
+package com.binghetao.interceptors;
+
+import com.binghetao.pojo.Result;
+import com.binghetao.utils.JwtUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.Map;
+
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String token = request.getHeader("Authorization");
+        try {
+            Map<String, Object> claims = JwtUtil.parseToken(token);
+            return true;
+        } catch (Exception e) {
+            response.setStatus(401);
+            return false;
+        }
+    }
+}
